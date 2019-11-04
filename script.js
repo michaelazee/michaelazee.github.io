e61f8c143e9bb885cf8d2e813315e6ba6c7dcadb
michaelazee <54605024+michaelazee@users.noreply.github.com>
2019-11-03 19:37:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.tap-target');
-    var instances = M.TapTarget.init(elems, options);
-  });
-
-  instance.next();
-  instance.next(3); // Move next n times.
-
- instance.close();
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.parallax');
+  var instances = M.Parallax.init(elems);
+});
